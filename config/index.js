42232e2e030e6ee14a465540c465dbdc5b857be8
Abdulghani Aljuhi <abdulghanialjuhi@Abdulghanis-MacBook-Pro.local>
2022-11-27 22:13:25 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const nextApi = dev ? 'http://localhost:3000' : 'https://next-movie-app-abdulghanialjuhi.vercel.app'
+export const nextApi = dev ? 'http://localhost:3000' : 'https://hci-movie.vercel.app'
 
 export const server = 'https://movie-back-end.herokuapp.com'
